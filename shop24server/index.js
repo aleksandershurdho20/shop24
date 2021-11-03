571660127c8577d371166f45b7390139148f8416
aleksandershurdho20 <aleksandershurdho@gmail.com>
2021-11-03 12:25:36 +0100

    wip

--- shop24server/index.js
+++ shop24server/index.js
@@ -3,11 +3,14 @@ const app = express();
 require("dotenv").config();
 const Database = require('./database/Database')
 const { readdirSync } = require("fs")
+const cors = require("cors");
 
 Database()
 readdirSync("./routes").map((route) =>
     app.use("/api", require(`./routes/${route}`))
 );
+app.use(cors());
+app.use(express.json());
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log('running on ', PORT)
