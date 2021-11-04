838324a91094382306eeaae0f4f23262db15eb56
aleksandershurdho20 <aleksandershurdho@gmail.com>
2021-11-04 12:37:49 +0100

    finished product api

--- shop24server/controllers/product.js
+++ shop24server/controllers/product.js
@@ -12,7 +12,68 @@ const createProduct = async (req, res) => {
     }
 }
 
+const updateProduct = async (req, res) => {
+    try {
+        const id = req.params.id
+        const updatedProduct = await Product.findByIdAndUpdate(id, {
+            $set: req.body
+        },
+            { new: true }
+        )
+        res.json({ updatedProduct })
+    } catch (error) {
+        res.status(500).send("Server error");
+    }
+}
+const getProduct = async (req, res) => {
+    try {
+        const id = req.params.id
+        const product = await Product.findById(id)
+        res.json({ product })
+    } catch (error) {
+        res.status(500).send("Server error");
 
+    }
+}
+const deleteProduct = async (req, res) => {
+    try {
+        const id = req.params.id
+        const product = await Product.findByIdAndDelete(id)
+        res.status(200).send("Product deleted!")
+    } catch (error) {
+        res.status(500).send("Server error");
+
+    }
+}
+const getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find({})
+        res.json({ products })
+    } catch (error) {
+        res.status(500).send("Server error");
+
+    }
+}
+
+const filterProducts = async (req, res) => {
+    try {
+        const category = req.query.category
+        const categories = await Product.find({
+            categories: {
+                $in: [category]
+            }
+        })
+        res.json({ categories })
+    } catch (error) {
+        res.status(500).send("Server error");
+
+    }
+}
 module.exports = {
-    createProduct
+    createProduct,
+    getProduct,
+    updateProduct,
+    deleteProduct,
+    getAllProducts,
+    filterProducts
 }
--- shop24server/routes/product.js
+++ shop24server/routes/product.js
@@ -1,8 +1,14 @@
 const express = require('express')
 
 const router = express.Router();
-const { createProduct } = require('../controllers/product.js')
+const { createProduct, updateProduct, getProduct, deleteProduct, getAllProducts, filterProducts } = require('../controllers/product.js')
 const { authorize } = require('../middlewares/verifyToken')
 
 router.post('/create/product', authorize, createProduct)
+router.put('/update/product/:id', authorize, updateProduct)
+router.get('/product/:id', authorize, getProduct)
+router.delete('/product/:id', authorize, deleteProduct)
+router.get('/products', authorize, getAllProducts)
+router.get('/products/categories', authorize, filterProducts)
+
 module.exports = router
