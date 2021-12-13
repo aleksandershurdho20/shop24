281438bbcb9059be7d33c88d87f79941961393df
aleksandershurdho20 <aleksandershurdho@gmail.com>
2021-12-13 17:55:40 +0100

    wip

--- shop24client/src/components/Support/ViewSupport/ViewSupport.jsx
+++ shop24client/src/components/Support/ViewSupport/ViewSupport.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Grid from '@mui/material/Grid';
 import ChatWidget from '../ChatWidget';
+import { apiInstance } from 'utils/api';
+import { useParams } from 'react-router-dom';
+
 export default function ViewSupport() {
+    const [messages,setMessages]=useState([])
+    const { id } = useParams();
+    useEffect(()=>{
+        apiInstance.get('/messages/'+id).then(res => setMessages(res.data.messages)).catch(err => console.log({err}))
+    },[id])
+    console.log({messages})
     return (
         <Grid container spacing ={2}>
 
@@ -9,7 +18,7 @@ export default function ViewSupport() {
                Container
             </Grid> 
             <Grid item md={3}>
-                <ChatWidget/>
+                <ChatWidget messages={messages}/>
             </Grid>
         </Grid>
     )
